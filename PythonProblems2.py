30b200bef191ef4b15984d7fa24db7140524e9bd
Luis Ramirez <40273183+reylui@users.noreply.github.com>
2019-06-09 18:01:02 -0700

    adding more code

--- PythonProblems2.py
+++ PythonProblems2.py
@@ -15,18 +15,49 @@ from IPython.core import page
 # First we take a look at the function that is going to help us model this DNA sequence evolution.
 # This is were most of the modeling takes place.
 
-from BioinformaticsCode.algorithms import evolve_sequence
+from algorithms import evolve_sequence
 
 # This function will help us model a single generation of a single sequence.
 # Here, clonal reproduction will occur
 
-from BioinformaticsCode.algorithms import evolve_generation
+from algorithms import evolve_generation
 
 # Then we have the entry point function. We provide the parameters for the simulation: Initial sequence, number of
 # generations and mutation probabilities.
 # This code is built on the previously mentioned functions.
 
-from BioinformaticsCode.algorithms import evolve_generations
+from algorithms import evolve_generations
+
+# now we can run a simulation using a random DNA sequence and evolve three generations. We will use evolve_sequence from
+# algorithms and pass the verbose = true parameter.
+
+from algorithms import random_sequence
+
+import skbio
+
+sequence = random_sequence(skbio.DNA, 50)
+
+sequences = evolve_generations(sequence, 3, 0.1, 0.05, 0.1, True)
+
+# sequences will contain the the child sequence from the last generation. We also note that we will only have sequences
+# from the last generation. Not the ancestral sequence, since this models a real world application.
+
+# Lets take a look at the following:
+
+print(len(sequences))
+
+print(sequences[0])
+
+print(sequences[-1])
+
+# In our simulation each, each sequence is derived from exactly one sequence from the previous generation, and the
+# evolution traces back to the ancestral sequence that we provided. This means that are final sequences are all
+# homologous. Our goal now, is to reconstruct the phylogeny given only the last generation of sequences. We will use the
+# fact that we know the previous phylogeny to evaluate our performance.
+
+
+
+
 
 
 
